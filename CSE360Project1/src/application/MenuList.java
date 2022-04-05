fed65301e7179c2e2d65566fdf75d3f4238ecd82
Ty Allison <94358717+CryoticNova@users.noreply.github.com>
2022-04-04 21:26:20 -0700

    Create MenuList.java

---
+++ CSE360Project1/src/application/MenuList.java
@@ -0,0 +1,12 @@
+package application;
+
+public class MenuList extends Menu
+{
+	
+	
+	MenuList(String Itemname, int ItemPrice)
+	{
+		this.Itemname = Itemname;
+		this.ItemPrice = ItemPrice;
+	}
+}
